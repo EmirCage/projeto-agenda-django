0974625da7ec932e511711bcf4b81d52f7a95ae8
Emir Eugenio <emireugenio3@gmail.com>
2025-01-29 17:30:22 -0300

    Adicionando todos os contatos no css e exibindo-os

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+from contact.models import Contact
+
 
 def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         request,
-        'contact/index.html'
+        'contact/index.html',
+        context
     )
