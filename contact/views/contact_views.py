1cf7f2eb96abd053a83f9b5d6f1b1b48b027dc0f
Emir Eugenio <emireugenio3@gmail.com>
2025-01-31 11:35:07 -0300

    Adicionando paginação na agenda e modificando urls.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-# from django.http import Http404
+from django.core.paginator import Paginator
 from django.db.models import Q
 from contact.models import Contact
 
@@ -7,10 +7,14 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[10:20]
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - ',
     }
 
@@ -37,9 +41,14 @@ def search(request):
         )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - ',
+        'search_value': search_value
     }
 
     return render(
